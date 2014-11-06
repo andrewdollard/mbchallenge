9a9bb33fb950a95b11054df744fc9bc04cf2f6ad
Andrew Dollard <andrewdollard@gmail.com>
2014-11-06 12:36:52 -0800

    Added axes and moved to timescale for x-axis

--- app/scripts/activity_view.js
+++ app/scripts/activity_view.js
@@ -3,32 +3,52 @@ var App = App || {};
 App.ActivityView = Backbone.View.extend({
 
   initialize: function(options) {
+    this._sampler = options.sampler;
     this._samples = options.sampler.samples();
+    this._timestamps = options.sampler.timestamps();
   },
 
   render: function() {
     var data = _.map(this._samples, function(sample) {
       return sample.percentActive();
     }),
+        self = this,
         width = 800,
         height = 400,
-        yScale = d3.scale.linear().domain([0, d3.max(data)]).range([0, height]),
-        xScale = d3.scale.linear().domain([0, data.length]).range([0 , width]);
+        padding = 40,
+        yScale = d3.scale.linear().domain([100, 0]).range([padding, height - padding]),
+        xScale = d3.time.scale().domain([this._sampler.startTime(), this._sampler.endTime()]).range([padding , width - padding]);
 
-    var vis = d3.select("body")
+    var field = d3.select("body")
         .append("svg:svg")
         .attr("width", width)
         .attr("height", height);
 
-    var g = vis.append("svg:g");
+    var activityLine = field.append("svg:g").attr('class', 'activityLine'),
+        line = d3.svg.line()
+                     .x(function(d,i) { return xScale(self._timestamps[i]) })
+                     .y(function(d) { return yScale(d); });
 
-    var line = d3.svg.line()
-        .x(function(d,i) { return xScale(i); })
-        .y(function(d) { return height - yScale(d); });
+    activityLine.append("svg:path") .attr("d", line(data));
 
-    g.append("svg:path").attr("d", line(data)).attr('stroke', 'dodgerblue')
-        .attr('stroke-width', 2)
-        .attr('fill', 'none');
+    var xAxis = d3.svg.axis()
+                      .scale(xScale)
+                      .orient('bottom');
+
+   field.append("svg:g")
+      .attr("class", "activityAxis")
+      .attr("transform", "translate(0," + (height - padding + 10) + ")")
+      .call(xAxis);
+
+
+    var yAxis = d3.svg.axis()
+                      .scale(yScale)
+                      .orient("left");
+
+    field.append("svg:g")
+        .attr("class", "activityAxis")
+        .attr("transform", "translate(" + (padding - 10) + ",0)")
+        .call(yAxis);
   }
 
 });
