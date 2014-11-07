1bccea8f1b4e26ce3a9e1183c75eea4e07dad85f
Andrew Dollard <andrewdollard@gmail.com>
2014-11-06 19:36:26 -0800

    setStartDate

--- app/scripts/app_controller.js
+++ app/scripts/app_controller.js
@@ -3,10 +3,10 @@ var App = App || {};
 App.controller = _.clone(Backbone.Events);
 
 App.controller.start = function(){
-  this._startDate = new Date('2014-07-28');
   this._days = [1,3,7,14];
-  this._periodsForDays = [App.constants.HOUR * 3, App.constants.HOUR * 3, App.constants.HOUR * 6, App.constants.HOUR * 6];
+  this._periodsForDays = [App.constants.HOUR, App.constants.HOUR * 3, App.constants.HOUR * 6, App.constants.HOUR * 6];
   this._currentDayIndex = 0;
+  this._setStartDate();
 
   var datePickerView = new App.DatePickerView({el: $('#date-picker-view'), days: this._days});
   datePickerView.render();
@@ -24,9 +24,13 @@ App.controller._updateActivityView = function() {
 
 App.controller.on('dateChange', function(day){
   this._currentDayIndex = _.indexOf(this._days, day);
-  var startDate = new Date(App.now),
-      dayOfMonth =  startDate.getDate();
-  startDate.setDate(dayOfMonth - day);
-  this._startDate = startDate;
+  this._setStartDate();
   this._updateActivityView();
 });
+
+App.controller._setStartDate = function(){
+  var startDate = new Date(App.now),
+      dayOfMonth =  startDate.getDate();
+  startDate.setDate(dayOfMonth - this._days[this._currentDayIndex]);
+  this._startDate = startDate;
+}
