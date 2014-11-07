0ba3fbcd305c3d6103559b9254960e60ef4c41ab
Andrew Dollard <andrewdollard@gmail.com>
2014-11-06 21:35:52 -0800

    DatePickerView is now extra responsive

--- app/scripts/date_picker_view.js
+++ app/scripts/date_picker_view.js
@@ -4,26 +4,38 @@ App.DatePickerView = Backbone.View.extend({
 
   initialize: function(options) {
    this._days = options.days;
+   this.$buttons = this.$el.find('.date-picker-buttons');
+   this.$select = this.$el.find('select');
   },
 
   events: {
-    'click .date-picker-button' : 'buttonClick'
+    'click .date-picker-button' : 'datePick',
+    'change select' : 'datePick'
   },
 
   render: function(){
-    this.$el.empty();
+    this.$buttons.empty();
     for (var i=0, len=this._days.length; i<len; i++){
       var text = (this._days[i] == 1) ? 'Today' : (this._days[i] + ' Days'),
           $button = $('<button>').text(text)
-                                 .attr('distance', this._days[i])
-                                 .addClass('date-picker-button btn btn-default');
-      this.$el.prepend($button);
+                                 .attr('value', this._days[i])
+                                 .addClass('date-picker-button btn btn-default'),
+          $option = $('<option>').text(text)
+                                  .attr('value', this._days[i])
+                                  .addClass('date-picker-select');
+
+      this.$buttons.append($button);
+      this.$select.append($option);
+      $option.on('select', function(e){
+        self.buttonClick();
+      });
     }
+
     this.delegateEvents();
   },
 
-  buttonClick: function(e) {
-    var d = parseInt($(e.target).attr('distance'), 10);
+  datePick: function(e) {
+    var d = parseInt($(e.target).val(), 10);
     App.controller.trigger('dateChange', d);
   }
 
