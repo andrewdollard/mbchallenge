24c5b64dfb90fa95f1e80172ea17bd2ff1aac6d9
Andrew Dollard <andrewdollard@gmail.com>
2014-11-06 22:01:10 -0800

    DatePicker mobile select works

--- app/scripts/date_picker_view.js
+++ app/scripts/date_picker_view.js
@@ -24,18 +24,24 @@ App.DatePickerView = Backbone.View.extend({
                                   .attr('value', this._days[i])
                                   .addClass('date-picker-select');
 
+      if (i == 0) { $button.addClass('selected'); }
       this.$buttons.append($button);
       this.$select.append($option);
       $option.on('select', function(e){
         self.buttonClick();
       });
     }
-
     this.delegateEvents();
   },
 
   datePick: function(e) {
-    var d = parseInt($(e.target).val(), 10);
+    var $t = $(e.target),
+        d = parseInt($t.val(), 10);
+
+    this.$buttons.find('.selected').removeClass('selected');
+    this.$buttons.find("[value='" + $t.val() + "']").addClass('selected');
+    this.$select.val($t.val());
+
     App.controller.trigger('dateChange', d);
   }
 
