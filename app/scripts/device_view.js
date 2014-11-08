d7db27404cbaf842e9cc24d64cafa84c20db1a17
Andrew Dollard <andrewdollard@gmail.com>
2014-11-07 21:05:04 -0800

    Cleaning up DeviceView and making it more responsive

--- app/scripts/device_view.js
+++ app/scripts/device_view.js
@@ -4,40 +4,50 @@ App.DeviceView = Backbone.View.extend({
 
   setSampler: function(sampler) {
     var samples = sampler.samples();
-    var values = sampler.values();
+    this._deviceNames = sampler.values();
     var eventCount = sampler.totalLength();
-    this._data = _(samples).map(function(d, i){ return [values[i], (samples[i].length / eventCount) * 100] });
+
+    this._data = _.map(sampler.samples(), function(sample){ return (sample.length / eventCount) * 100 });
+
+    var self = this;
+    $(window).on('resize', function(){
+      self.render();
+    });
   },
 
   render: function(){
-    var padding = [0, 180, 0, 180];
+    this._configureDimensions();
+    this._draw();
+  },
+
+  _configureDimensions: function(){
+    this._width = this.$el.width();
+    this._padding = [20, 110, 0, 110];
+    var diameter = this._width - this._padding[1] - this._padding[3];
+    this._outerRadius = diameter / 2;
+    this._floatingLabelRadius = this._outerRadius + 60;
+    this._innerLabelRadius = this._outerRadius / 2;
+  },
+
+  _draw: function(){
     var self = this;
-
-    var containerWidth = this.$el.width();
-    var diameter = containerWidth - padding[1] - padding[3];
-    var outerRadius = diameter / 2;
-    var floatingLabelRadius = outerRadius + 60;
-    var innerLabelRadius = outerRadius / 2;
-
     var pie = d3.layout.pie();
-    var pieData = _.map(this._data, function(d){ return d[1] });
     var arc = d3.svg.arc()
         .innerRadius(0)
-        .outerRadius(outerRadius);
-
+        .outerRadius(this._outerRadius);
 
     this.$el.empty();
     var svg = d3.select(this.el)
         .append("svg")
-        .attr("width", containerWidth)
-        .attr("height", containerWidth);
+        .attr("width", this._width)
+        .attr("height", this._width);
 
     var arcs = svg.selectAll("g.arc")
-        .data(pie(pieData))
+        .data(pie(this._data))
         .enter()
         .append("g")
         .attr("class", "arc")
-        .attr("transform", "translate(" + (outerRadius + padding[3]) + ", " + (outerRadius + padding[0]) + ")");
+        .attr("transform", "translate(" + (this._outerRadius + this._padding[3]) + ", " + (this._outerRadius + this._padding[0]) + ")");
 
     var color = d3.scale.category10();
     arcs.append("path")
@@ -52,16 +62,15 @@ App.DeviceView = Backbone.View.extend({
               x = c[0],
               y = c[1],
               h = Math.sqrt(x*x + y*y);
-          return "translate(" + ((x/h * floatingLabelRadius) - 40) +  ',' +
-              ((y/h * floatingLabelRadius) - 20) +  ")";
+          return "translate(" + ((x/h * self._floatingLabelRadius) - 40) +  ',' +
+              ((y/h * self._floatingLabelRadius) - 20) +  ")";
         })
         .attr('width', '80')
         .attr('height', '30')
         .attr('rx', '5').attr('ry', '5')
         .attr("fill", function(d, i) {
           return color(i);
-        })
-    ;
+        });
 
     arcs.append("svg:text")
         .attr("transform", function(d) {
@@ -69,11 +78,11 @@ App.DeviceView = Backbone.View.extend({
               x = c[0],
               y = c[1],
               h = Math.sqrt(x*x + y*y);
-          return "translate(" + (x/h * floatingLabelRadius) +  ',' +
-              (y/h * floatingLabelRadius) +  ")";
+          return "translate(" + (x/h * self._floatingLabelRadius) +  ',' +
+              (y/h * self._floatingLabelRadius) +  ")";
         })
         .attr("text-anchor", "middle")
-        .text(function(d, i) { return self._data[i][0] });
+        .text(function(d, i) { return self._deviceNames[i] });
 
     arcs.append("svg:text")
         .attr("transform", function(d) {
@@ -81,8 +90,8 @@ App.DeviceView = Backbone.View.extend({
               x = c[0],
               y = c[1],
               h = Math.sqrt(x*x + y*y);
-          return "translate(" + (x/h * innerLabelRadius) +  ',' +
-              (y/h * innerLabelRadius) +  ")";
+          return "translate(" + (x/h * self._innerLabelRadius) +  ',' +
+              (y/h * self._innerLabelRadius) +  ")";
         })
         .attr("class", "pie-label-text")
         .attr("text-anchor", "middle")
