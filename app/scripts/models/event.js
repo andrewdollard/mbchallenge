a9e4b22feb5a203f2113a36debe832b13a00bdfd
Andrew Dollard <andrewdollard@gmail.com>
2014-11-09 14:02:03 -0800

    Fixing some cross-browser issues

--- app/scripts/models/event.js
+++ app/scripts/models/event.js
@@ -1,10 +1,11 @@
 var App = App || {};
 
+App.dateFormat = '%Y-%m-%d %X %Z';
 App.Event = Backbone.Model.extend({
 
   parse: function(response) {
-    response.timestamp = new Date(response.timestamp);
+    response.timestamp = d3.time.format(App.dateFormat).parse(response.timestamp);
     return response;
   }
-
+  
 });
